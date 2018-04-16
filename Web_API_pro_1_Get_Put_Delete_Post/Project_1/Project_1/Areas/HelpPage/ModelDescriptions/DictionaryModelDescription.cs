cfc11fceab553b454fda8494b0d2889c6f7a7ccb
Masimba Gomba Ndadzibaya <masimban@sahomeloans.com>
2018-04-16 11:22:17 +0200

    Updated the first project with Get, Put, Post and Delete HTTP VERBS. Use fiddleer to play around with the API

---
+++ Web_API_pro_1_Get_Put_Delete_Post/Project_1/Project_1/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Project_1.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Web_API_pro_1_Get_Put_Delete_Post/Project_1/Project_1/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Project_1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public static List<string> strings = new List<string>()
+        {
+            "Value-1","Value-2","Value-3","Value-4"
+        };
+        // GET api/values
+        public List<string> Get()
+        {
+            return strings;
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        public string Get(int id)
+        {
+            return strings[id];
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            strings.Add(value);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+            strings[id] = value;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            strings.RemoveAt(id);
+        }
+    }
+}
